71e31dff3393c2da04251d7b07a1397500c7a9fa
Thomas Fady <thomas.fady@zenika.com>
2020-03-26 15:19:33 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/thomasfady/httpfiletransfer
+
+go 1.13
+
+require (
+	github.com/gin-contrib/sse v0.1.1-0.20190905051334-43f0f29dbd2b
+	github.com/gin-gonic/gin v1.3.0
+	github.com/golang/protobuf v1.3.5
+	github.com/json-iterator/go v1.1.10-0.20200326113732-b22f3938580a
+	github.com/mattn/go-isatty v0.0.13-0.20200128103942-cb30d6282491
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd
+	github.com/modern-go/reflect2 v1.0.1
+	github.com/ugorji/go v1.1.8-0.20190812104308-42bc974514ff
+	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd
+	gopkg.in/go-playground/validator.v8 v8.18.2
+	gopkg.in/yaml.v2 v2.2.8
+)
---
+++ main.go
@@ -0,0 +1,51 @@
+package httpfiletransfer
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+var uploadDir = "uploads"
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.NoRoute(func(c *gin.Context) {
+		c.String(http.StatusOK, "")
+	})
+	r.PUT("/*filepath", func(c *gin.Context) {
+		filePath := c.Param("filepath")
+		baseDir := filepath.Join(uploadDir, c.ClientIP())
+		finalPath := filepath.Join(baseDir, filepath.Clean(filePath))
+		if strings.HasPrefix(filepath.Clean(finalPath), baseDir) {
+			if _, err := os.Stat(filepath.Dir(finalPath)); os.IsNotExist(err) {
+				os.MkdirAll(filepath.Dir(finalPath), 0700)
+			}
+			file, err := os.Create(finalPath)
+			if err != nil {
+				return
+			}
+			defer file.Close()
+			data, _ := c.GetRawData()
+			file.Write(data)
+			c.String(http.StatusOK, "OK")
+		} else {
+			c.String(http.StatusForbidden, "")
+		}
+
+	})
+	r.Static("", "./")
+	return r
+}
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	r := setupRouter()
+	log.Print("Listening on :8080")
+	r.Run(":8080")
+}
